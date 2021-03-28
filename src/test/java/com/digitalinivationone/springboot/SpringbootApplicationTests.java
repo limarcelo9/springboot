f334b7a9c39194ee593bd91f9634492e81129889
unknown <limarcelo90@gmail.com>
2021-03-28 00:03:42 -0400

    commit inicial

---
+++ src/test/java/com/digitalinivationone/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinivationone.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
